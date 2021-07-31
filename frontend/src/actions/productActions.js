21c0ccba24adabf0c3729b0c12e1d07ab0c6ec1f
unknown <anilaloorjoseph@gmail.com>
2021-07-31 09:57:56 -0700

    bugs in productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -31,10 +31,7 @@ export const listProductDetails = (id) => async (dispatch) => {
     dispatch({
       type: "PRODUCT_DETAILS_REQUEST",
     });
-    const { data } = await axios.get(` /
-          api /
-          products /
-          ${id}`);
+    const { data } = await axios.get(`/api/products/${id}`);
 
     dispatch({
       type: "PRODUCT_DETAILS_SUCCESS",
